7fe5b1f049e6bcfe241164d30ef312e67b96a7a1
subject-name-here <jura.paradov@gmail.com>
2018-05-02 11:13:29 +0300

    cleand up the mess

--- script.sh
+++ script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+mkdir replayz
+cd replay_getter
+
 python3 scrapper.py $1
 python3.5 get_links.py
 python3.5 downloader.py
 
-rm -rf ../replayz
-mkdir ../replayz
 > match_id.txt
 > match_links.txt
+
+cd ..
+
+rm -rf replayz
+
+
