337e8074b7056c09605b58d8354836400d9e86a1
subject-name-here <jura.paradov@gmail.com>
2018-05-21 20:43:32 +0300

    update script

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+rm -rf replayz
 mkdir replayz
+
 cd replay_getter
+
+> match_id.txt
+> match_links.txt
 
 python3 scrapper.py $1
 python3.5 get_links.py
 python3.5 downloader.py
 
-> match_id.txt
-> match_links.txt
-
 cd ..
 
-rm -rf replayz
+
 
 
